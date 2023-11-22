fa9de46ca89731a31ada160779d75b2f8cabad95
MHS12 <110741580+MHS12@users.noreply.github.com>
2023-11-22 21:49:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const icon = document.querySelector(".icons");
+const menu =document.querySelector(".headbar");
+const but =document.querySelector("button");
+const v=document.getElementsByName("icbar")
+icon.addEventListener('click',()=> {
+
+   menu.classList.toggle('active')
+})
