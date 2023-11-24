a75409ae3693f8d898283e3d6b189488fd6d0541
MHS12 <110741580+MHS12@users.noreply.github.com>
2023-11-24 20:12:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const icon = document.querySelector(".icons");
-const menu =document.querySelector(".headbar");
-const but =document.querySelector("button");
-const v=document.getElementsByName("icbar")
-icon.addEventListener('click',()=> {
-
-   menu.classList.toggle('active')
-})
